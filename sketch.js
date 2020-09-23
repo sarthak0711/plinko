76949872ccef464ce9091e58c701bddeef08b20c
sarthak <skothia07@gmail.com>
2020-09-23 16:55:42 +0530

    plinko

--- sketch.js
+++ sketch.js
@@ -1,18 +1,26 @@
-var Engine = Matter.Engine,
-  World = Matter.World,
-  Events = Matter.Events,
-  Bodies = Matter.Bodies;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Event = Matter.Event;
  
 var particles = [];
+var particles,divisions;
 var plinkos = [];
-
+var divisions = []
 var divisionHeight=300;
-var score =0;
+var score = 0;
+var turn=0;
+var gameState=1
+var PLAY=1
+var END=0
+
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
-  ground = new Ground(width/2,height,width,20);
+
+  ground = new Ground(width/2,height,width,20,20);
+  //divisions
 
 
    for (var k = 0; k <=width; k = k + 80) {
@@ -54,9 +62,10 @@ function setup() {
 function draw() {
   background("black");
   textSize(20)
- //text("Score : "+score,20,30);
+ text("Score : "+score,20,30);
   Engine.update(engine);
  
+ if( gameState=PLAY){
   
    for (var i = 0; i < plinkos.length; i++) {
      
@@ -64,8 +73,9 @@ function draw() {
      
    }
    if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
      score++;
+     //particles.display();
    }
  
   for (var j = 0; j < particles.length; j++) {
@@ -76,4 +86,21 @@ function draw() {
      
      divisions[k].display();
    }
+
+   if(score>=500 || score>=200 || score>=100){
+     gameState=END
+   }
+  }
+
+   
+  
+}
+
+function mousePressed(){
+
+  if(gameState!== PLAY){
+   count++;
+    particle = new Particle(mouseX,10,10,10);
+  }
+
 }
